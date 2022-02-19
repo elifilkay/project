d31b1ecabfaa2f49dbe8f8c665914f779132c631
elifilkay <93220459+elifilkay@users.noreply.github.com>
2022-02-19 15:49:35 +0300

    Add files via upload
    Maaş hesaplayan sistem

---
+++ MainEmployee.java
@@ -0,0 +1,13 @@
+
+
+public class MainEmployee {
+    public static void main(String[] args) {
+        EmployeeYeni a=new EmployeeYeni("İlkay",10000,50,2015);
+        EmployeeYeni b=new EmployeeYeni("Yasin",950.0,70,2001);
+        a.ToString();
+        System.out.println("**************");
+        b.ToString();
+
+
+    }
+}
